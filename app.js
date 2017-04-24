8f37a9350b7a77b0d92c67700f23093cfc459c0f
oden king <odenking@gmail.com>
2017-04-24 19:36:48 +0800

    modify app.js #

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(document);
     }
-    #console.log("http...");
+    //console.log("http...");
 });
 var connections=0;
 const wss = new WebSocketServer({ server: server });
 wss.messages=[];
 wss.on('connection', (ws) => {
-    #console.log("start connection");
+    //console.log("start connection");
     
     connections++; 
     var tmp="S "+connections+" "+getKb();
@@ -27,7 +27,7 @@ wss.on('connection', (ws) => {
     wss.messages.forEach(function(msg) {
         ws.send(msg);
     });
-    #console.log('connections: ' + connections);
+    //console.log('connections: ' + connections);
     ws.on('message',onMessage);
     ws.on('error', function() {
         console.log("error");
@@ -37,8 +37,8 @@ wss.on('connection', (ws) => {
         connections--;
         var tmp="S "+connections+" "+getKb();
         wss.broadcast(tmp);
-        #console.log('connections: ' + connections);
-        #console.log('Connection closed')
+        //console.log('connections: ' + connections);
+        //console.log('Connection closed')
     })
 });
 wss.broadcast = function broadcast(data) {
@@ -49,7 +49,7 @@ wss.broadcast = function broadcast(data) {
   });
 };
 function onMessage(message) {
-      #console.log('received: ' + message);
+      //console.log('received: ' + message);
       wss.messages.push(message);
       
       wss.clients.forEach(function each(client) {
@@ -62,12 +62,12 @@ function onMessage(message) {
 }
 function getKb() {
     var size=0;
-    #console.log(wss.messages);
+    //console.log(wss.messages);
     wss.messages.forEach(function(msg) {
         size += Buffer.byteLength(msg, 'utf8')
-        #console.log("loop:"+size);
+       // console.log("loop:"+size);
     });
-    #console.log(size+"=>"+size/1024);
+    //console.log(size+"=>"+size/1024);
     return size/1024;
 }
 
